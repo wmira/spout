bba9cf609f7ce9ce21d93cd74423c92e28f69f30
Warren Mira <warrenmira@gmail.com>
2015-01-24 23:52:55 +0800

    updated build

--- js/spout.js
+++ js/spout.js
@@ -54,129 +54,140 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/** @jsx React.DOM */
 	/*globals require,module */
+	/* jshint -W097 */
 	"use strict";
 
 	var EventEmitter = __webpack_require__(1).EventEmitter;
-	/**
-	 * 
-	 *
-	 */
+
+	var Store = function(pconstr,pattributes) {
+
+	    var constr;
+	    var attributes;
+
+	    if ( pconstr && typeof pconstr === 'function' ) {
+	        constr = pconstr;
+	    } else if ( pconstr && typeof pconstr === 'object' ) {
+	        attributes = pconstr;
+	    }
+
+	    if ( !attributes ) {
+	        attributes = pattributes || {};
+	    }
+
+	    Object.keys(attributes).forEach(function(key) {
+	        if ( attributes[key] !== null || attributes[key] !== undefined ) {
+	            this[key] = Object(attributes[key]);
+	        }
+	    }.bind(this));
+
+	    if ( constr ) {
+	        constr.call(this);
+	    }
+	    EventEmitter.apply(this);
+	    return this;
+	};
+
+	Store.prototype = Object.create(EventEmitter.prototype);
+
+
+
+	var StoreFactory = function(pconstr,pattributes) {
+	    return  new Store(pconstr,pattributes);
+	};
+
+
 	var Dispatcher = function() {
+	    var dispatchers = [];
 	    
 	    var consumers = [];
 
 	    /**
 	     * Register a consumer
-	     *  
+	     *
 	     * @param consumer
 	     */
 	    this.register = function(consumer) {
+	        
 	        consumers.push(consumer);
 	    };
 
 	    /**
 	     * Dispatch the given payload
-	     * 
-	     *  
-	     *  
+	     *
+	     *
+	     *
 	     * @param payload
 	     */
 	    this.dispatch = function(payload) {
 	        consumers.forEach(function(consumer) {
-	            consumer(payload);
+	            if ( consumer.dispatch ) {
+	                consumer.dispatch(payload);
+	            } else {
+	                consumer(payload);
+	            } 
 	        });
 	    };
-	    
-	};
 
-	var Store =  function(name,dispatcher,storeAttributes)  {
-	    this.name = name;
-	    this.dispatcher = dispatcher;
 
-	    console.log(storeAttributes);
-	    Object.keys(storeAttributes).forEach(function(key) {
-	        this[key] = storeAttributes[key];
-	    }.bind(this));
-	    
-	    if ( this.dispatch ) {
-	        this.dispatcher.register(this.dispatch);
-	    } else {
-	        console.log("Warning: dispatch method needed to do automatic registration");
-	    }
+	    /**
+	     * Create a click dispatcher for this el
+	     * 
+	     * @param el
+	     */
+	    this.clickDispatcher = function(el) {
+	        dispatchers.push(new ClickDispatcher(el,this));
+	        return this;
+	    };
 	};
-	    
-	Store.prototype = Object.create(EventEmitter.prototype);
-	    
-	    
 
 	/**
-	 * A dispatcher that generates dispatches based on clicks
-	 *  
+	 * Listens to all clicks and dispatches as needed
+	 *
 	 * @param el
 	 * @constructor
 	 */
 	var ClickDispatcher = function(el,mainDispatcher) {
-	    
+
 	    this.mainDispatcher = mainDispatcher;
 	    this.el = el;
-	    
-	    
-	    this.dispatchClick = function(e) {
-	        var target = e.target;
-	        var action = target.getAttribute("data-action");
+
+	    this.el.addEventListener("click",function(e) {
 	        
+	        var target = e.target;
+	        var action = target.getAttribute("dispatch-action") || target.getAttribute("data-dispatch-action");
+	        var source = target.getAttribute("dispatch-source")|| target.getAttribute("data-dispatch-source");
+	        var payload;
 	        if ( !action ) {
 	            return;
 	        }
-	        var source = target.getAttribute("data-source");
-	        
-	        var payload = {
+
+
+	        payload = {
 	            source : source,
 	            action : {
 	                type : action
 	                //TODO: collect all data-c- attribs and pass it
 	            }
 	        };
+	        
 	        this.mainDispatcher.dispatch(payload);
-	    };
-	    
-	    this.el.addEventListener("click", this.dispatchClick.bind(this));
+	    }.bind(this));
+
+
 	};
 
-	ClickDispatcher.prototype = Object.create(Dispatcher.prototype);
-
-
-	/**
-	 * Create an instance using the given el as the base or document.body if null
-	 *
-	 *  
-	 */
-	var Spout = function(el) {
+	module.exports = {
+	    store : function(construct,attribs) {
+	        return new StoreFactory(construct,attribs);
+	    },
+	    dispatcher : function() {
+	        return new Dispatcher();
+	    },
 	    
-	    this.stores = {};
-	    
-	    this.el = el || document.body;
-	    
-	    this.dispatcher = new Dispatcher();
-	    this.clickDispatcher = new ClickDispatcher(this.el,this.dispatcher);
-
-	    /**
-	     * 
-	     *  
-	     * @param storeName
-	     */
-	    this.store = function(storeName,storeFunctions) {
-	        var theStore = new Store(storeName,this.dispatcher,storeFunctions);
-	        this.stores[storeName] = theStore;
-	        return this.stores[storeName];
-	    };
-	    
-	};
-
-	module.exports = function(el) {
-	  return new Spout(el);
+	    emitter : function() {
+	        return new EventEmitter();
+	    }
 	};
 
 
